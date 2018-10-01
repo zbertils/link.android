5311567e098fce533ad655ff4b57db866bef4e29
zbertils <31749793+zbertils@users.noreply.github.com>
2018-10-01 16:25:47 -0500

    Show dtc status with colors.
     - Colors help show importance of the code status.

--- app/src/main/java/beze/link/ui/DtcStatusRecyclerViewAdapter.java
+++ app/src/main/java/beze/link/ui/DtcStatusRecyclerViewAdapter.java
@@ -1,5 +1,7 @@
 package beze.link.ui;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,6 +54,31 @@ public class DtcStatusRecyclerViewAdapter extends RecyclerView.Adapter<DtcStatus
         holder.dtcDescription.setText(dtc.Description);
         holder.dtcType.setText(dtc.Computer.toString());
         holder.dtcStatus.setText(dtc.Status);
+
+        // reset all of the colors
+        holder.dtcNumber.setTextColor(Color.BLACK);
+        holder.dtcDescription.setTextColor(Color.LTGRAY);
+        holder.dtcType.setTextColor(Color.BLACK);
+        holder.dtcStatus.setTextColor(Color.LTGRAY);
+
+        // set styles dependent on the status value
+        if (dtc.Status.contains("Warning lamp illuminated"))
+        {
+            holder.dtcNumber.setTextColor(Color.RED);
+            holder.dtcNumber.setTypeface(holder.dtcNumber.getTypeface(), Typeface.BOLD);
+        }
+        else if (dtc.Status.contains("Warning lamp pending"))
+        {
+            holder.dtcNumber.setTextColor(Color.MAGENTA);
+        }
+        else
+        {
+            // no lamp, not pending, either a stored non-critical code, previously lit, or not enough data
+            holder.dtcNumber.setTextColor(Color.LTGRAY);
+            holder.dtcDescription.setTextColor(Color.LTGRAY);
+            holder.dtcType.setTextColor(Color.LTGRAY);
+            holder.dtcStatus.setTextColor(Color.LTGRAY);
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
