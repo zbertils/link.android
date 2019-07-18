440cf88aeb1c9e7bd45222b085eecfa9e33c4773
zbertils <31749793+zbertils@users.noreply.github.com>
2019-07-17 20:36:01 -0500

    Properly follow log setting value.

--- app/src/main/java/beze/link/util/LogPusher.java
+++ app/src/main/java/beze/link/util/LogPusher.java
@@ -1,30 +1,46 @@
 package beze.link.util;
 
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+
 import com.hypertrack.hyperlog.HyperLog;
 
 import beze.link.Globals;
 
 public class LogPusher extends WorkerThread
 {
-
+    private final String TAG = Globals.TAG_BASE + "LogPusher";
     long maxLogCount = 500;
     LogPusherCallback callback = new LogPusherCallback();
 
     @Override
     protected void doWork()
     {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(Globals.mainActivity.getApplicationContext());
+        boolean sendLogs = sharedPref.getBoolean(Globals.Preferences.KEY_PREF_SEND_LOGCAT, false);
+        HyperLog.v(TAG, "Logs will" + ((sendLogs) ? "" : " NOT") + " be sent to developer");
+
         while (!stopWork)
         {
             long count = HyperLog.getDeviceLogsCount();
             if (count > maxLogCount)
             {
-                HyperLog.pushLogs(Globals.mainActivity, false, callback);
-
-                while (!callback.isResponseReceived())
+                if (sendLogs)
                 {
-                    // FIXME: there is a possibilty of losing logs if new logs are generated
-                    // between the push and when the the success callback is received
-                    try { Thread.sleep(100); } catch (Exception e) {}
+                    HyperLog.pushLogs(Globals.mainActivity, false, callback);
+
+                    while (!callback.isResponseReceived())
+                    {
+                        // FIXME: there is a possibilty of losing logs if new logs are generated
+                        // between the push and when the the success callback is received
+                        try
+                        {
+                            Thread.sleep(100);
+                        }
+                        catch (Exception e)
+                        {
+                        }
+                    }
                 }
                 HyperLog.getDeviceLogs(true); // clear out old logs
                 callback.reset();
@@ -35,13 +51,22 @@ public class LogPusher extends WorkerThread
             }
         }
 
-        HyperLog.pushLogs(Globals.mainActivity, false, callback);
-
-        while (!callback.isResponseReceived())
+        if (sendLogs)
         {
-            // FIXME: there is a possibilty of losing logs if new logs are generated
-            // between the push and when the the success callback is received
-            try { Thread.sleep(100); } catch (Exception e) {}
+            HyperLog.pushLogs(Globals.mainActivity, false, callback);
+
+            while (!callback.isResponseReceived())
+            {
+                // FIXME: there is a possibilty of losing logs if new logs are generated
+                // between the push and when the the success callback is received
+                try
+                {
+                    Thread.sleep(100);
+                }
+                catch (Exception e)
+                {
+                }
+            }
         }
         HyperLog.getDeviceLogs(true); // clear out old logs
         callback.reset();
